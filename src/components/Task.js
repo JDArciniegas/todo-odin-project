7d2f1f6686f0d79b6e5b68ce4965f2ee6519ce58
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-03-10 23:34:37 -0700

    created Task class

---
+++ src/components/Task.js
@@ -0,0 +1,12 @@
+class Task {
+  constructor(name, description, dueDate, priority){
+    this.name = name,
+    this.description = description,
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+const newTask = new Task('laundry', 'doing laundry', 'now', 'high');
+console.log(newTask);
+export default Task;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Testing");
+import Task from "./components/Task";
+
+const newTask = new Task("laundry", "doing laundry", "now", "high");
+console.log(newTask);
+
+
+document.getElementById('content');
+
