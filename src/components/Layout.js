9b9d2adb19ce1c9d0485fd27980beb5105813fd4
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-03-20 22:44:12 -0600

    starting layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,35 +1,51 @@
 import Project from "./Project";
 
-const tastProject = new Project("My Todo List");
-tastProject.add("Buy groceries", "Buy milk, bread and eggs", "2023-03-25", "High");
-tastProject.add(
-  "Pay bills",
-  "Pay rent and electricity bills",
-  "2023-04-01",
-  "Medium"
-);
-tastProject.add(
-  "Complete project",
-  "Finish project before deadline",
-  "2023-04-10",
-  "High"
-);
+// default starting projects
+const homeProject = new Project("Home");
+homeProject.add('Laundry','Laundry Day', '2023-04-01','high');
+homeProject.add("Cook", "Dinner", "2023-04-01", "medium");
 
 
-const displayProjectList = (list) => {
-  const display = document.createElement('ul');
-  Object.values(list.projectList).forEach((value) => {
-    let item = document.createElement('li');
-    item.textContent = value.name
-    display.appendChild(item);
-  })
-  return display;
+const createHeader = () => {
+  const header = document.createElement('header');
+  const h1 = document.createElement('h1');
+  h1.textContent = "Todo List"
+  header.appendChild(h1);
+  return header
 }
 
+const contentContainer = () => {
+  const contentContainer = document.createElement('div');
+  const projectNameContainer = document.createElement('div');
+  const projectListContainer = document.createElement("div");
+  projectNameContainer.append(displayProjectName(homeProject));
+  projectListContainer.append(displayProjectList(homeProject))
+  contentContainer.appendChild(projectNameContainer);
+  contentContainer.appendChild(projectListContainer);
+  return contentContainer;
+}
+
+const displayProjectName = (project) => {
+  const projectName = document.createElement("p");
+  projectName.textContent = project.name;
+  return projectName;
+};
+
+const displayProjectList = (project) => {
+  const display = document.createElement("ul");
+  Object.values(project.projectList).forEach((value) => {
+    let item = document.createElement("li");
+    item.textContent = value.name;
+    display.appendChild(item);
+  });
+  return display;
+};
 
 const Layout = () => {
   const homeLayout = document.createElement("div");
-  homeLayout.append(displayProjectList(tastProject));
+  homeLayout.append(createHeader());
+  homeLayout.append(contentContainer());
+
   return homeLayout;
 };
 
--- src/components/ProjectUI.js
+++
@@ -1,9 +0,0 @@
-import Project from "./Project";
-
-let defaultProjects = ["Home", "Work", "School"];
-
-const ProjectUI = () => {
-  return allProjects(defaultProjects);
-};
-
-export default ProjectUI;
