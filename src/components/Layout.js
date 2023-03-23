31c460c06d57576efbbb3c2f613fcf88dea484bc
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-03-23 15:34:11 -0600

    created findProject and added allprojects array

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,13 @@
 import ProjectUI from "./ProjectUI";
+const addTaskButton = document.querySelector('#add-task');
+
+const taskForm = document.querySelector('#task-form');
+
+const taskContainer = document.querySelector('#project-task');
+
+addTaskButton.addEventListener('click', (e) => {
+  taskForm.classList.remove('hidden')
+})
 
 const Layout = () => {
   ProjectUI();
--- src/components/ProjectUI.js
+++ src/components/ProjectUI.js
@@ -2,8 +2,14 @@ import Project from "./Project";
 
 const ProjectUI = () => {
   // project names container
-  const projects = document.querySelector("#project-names");
-  const projectList = document.querySelector("#project-list");
+  let allProjects = [];
+  let homeProject = new Project("Home");
+  homeProject.add("laundry", "do laundry", "2023-05-01", "low");
+  let workProject = new Project("work");
+  workProject.add("report", "ninja report", "2023-04-01", "high");
+  allProjects.push(homeProject);
+  allProjects.push(workProject);
+  const projectListNames = document.querySelector("#project-list");
 
   // add project form
   const projectForm = document.querySelector("#project-form");
@@ -12,26 +18,47 @@ const ProjectUI = () => {
   // buttons
   const addProjectButton = document.querySelector("#add-new-project");
 
+  const createProject = (project) => {
+    let newProject = new Project(project);
+    return newProject;
+  };
+
+  const addProjectToList = (allProjects) => {
+    allProjects.forEach((item) => {
+      let listItem = document.createElement("li");
+      listItem.textContent = item.name;
+      projectListNames.append(listItem);
+    });
+    return projectListNames;
+  };
+
+  addProjectToList(allProjects);
+
+  const findProject = () => {
+    allProjects.forEach((project) => {
+      Object.keys(project).find((key) => {
+        if(project[key] === "Home"){
+          console.log(project);
+        };
+      })
+    })
+  };
+
+  findProject();
+
   // ****** EVENTS *****
   addProjectButton.addEventListener("click", (e) => {
     projectForm.classList.remove("hidden");
+  });
+
+  projectListNames.addEventListener("click", (e) => {
+    console.log(e.target);
   });
 
   projectForm.addEventListener("submit", (e) => {
     e.preventDefault();
     addProjectToList(projectName.value);
   });
-
-  const addProjectToList = (project) => {
-    let newProject = new Project(project);
-    let listItem = document.createElement("li");
-    listItem.textContent = newProject.name;
-    projectList.append(listItem);
-  };
-
-  addProjectToList("Home");
-  addProjectToList("Work");
-  addProjectToList("Gym");
 };
 
 export default ProjectUI;
