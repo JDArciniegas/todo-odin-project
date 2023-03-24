4b9cca800c3a0d2c8d04c057ab196110fd6c1928
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-03-24 00:05:40 -0600

    added display Tasks loop and find Project

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,4 @@
 import ProjectUI from "./ProjectUI";
-const addTaskButton = document.querySelector('#add-task');
-
-const taskForm = document.querySelector('#task-form');
-
-const taskContainer = document.querySelector('#project-task');
-
-addTaskButton.addEventListener('click', (e) => {
-  taskForm.classList.remove('hidden')
-})
 
 const Layout = () => {
   ProjectUI();
--- src/components/ProjectUI.js
+++ src/components/ProjectUI.js
@@ -5,6 +5,8 @@ const ProjectUI = () => {
   // project names container
   // .add will create a task and add to corresponding project
   let allProjects = [];
+  let currentProject;
+
   let homeProject = new Project("Home");
   homeProject.add("laundry", "do laundry", "2023-05-01", "low");
   homeProject.add("cooking", "Make Dinner", "2023-04-01", "high");
@@ -20,10 +22,20 @@ const ProjectUI = () => {
   const projectListNames = document.querySelector("#project-list");
   // add project form
   const projectForm = document.querySelector("#project-form");
+  // project input
   const projectName = document.querySelector("#project-name");
+  // task form
+  const taskForm = document.querySelector("#task-form");
 
+  // task inputs
+  const taskName = document.querySelector("#task-name");
+  const taskDescription = document.querySelector("#task-description");
+  const taskDate = document.querySelector("#task-date");
+  const taskPriority = document.querySelector("#task-priority");
+  const taskSubmit = document.querySelector("#task-submit");
   // buttons
   const addProjectButton = document.querySelector("#add-new-project");
+  const addTaskButton = document.querySelector("#add-task");
 
   const createProject = (project) => {
     let newProject = new Project(project);
@@ -39,12 +51,12 @@ const ProjectUI = () => {
   };
 
   const displayTasks = (tasks) => {
-    const taskList = document.createElement('ul')
+    const taskList = document.createElement("ul");
     tasks.forEach((task) => {
-      let listItem = document.createElement('li');
-      listItem.textContent = `${task.name} - ${task.description} - ${task.dueDate} - ${task.priority}`
+      let listItem = document.createElement("li");
+      listItem.textContent = `${task.name} - ${task.description} - ${task.dueDate} - ${task.priority}`;
       taskList.appendChild(listItem);
-    })
+    });
     projectTasks.appendChild(taskList);
   };
 
@@ -56,19 +68,43 @@ const ProjectUI = () => {
     });
   };
 
+  const findProject = (projects, target) => {
+    projects.find((project) => {
+      if(project.name.toLowerCase() === target){
+        currentProject = project;
+      };
+    })
+  }
+
   // ****** EVENTS *****
   addProjectButton.addEventListener("click", (e) => {
     projectForm.classList.remove("hidden");
   });
 
   projectListNames.addEventListener("click", (e) => {
+    addTaskButton.classList.remove('hidden');
+    let hah = findTasks(allProjects, e.target.textContent.toLowerCase());
     findTasks(allProjects, e.target.textContent.toLowerCase());
+    findProject(allProjects, e.target.textContent.toLowerCase());
   });
 
   projectForm.addEventListener("submit", (e) => {
     e.preventDefault();
     createProject(projectName.value);
+    projectListNames.innerHTML = '';
     displayProjectNames(allProjects);
+  });
+
+  addTaskButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    taskForm.classList.remove("hidden");
+  });
+
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    currentProject.add('1,2,3,4')
+    console.log(currentProject);
+    displayTasks(currentProject.projectList);
   });
 
   displayProjectNames(allProjects);
