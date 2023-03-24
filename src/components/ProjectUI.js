3394738e0ab822ebca721b65e3dcddcdd57779a2
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-03-23 22:53:13 -0600

    added Task Display function

--- src/components/ProjectUI.js
+++ src/components/ProjectUI.js
@@ -1,16 +1,22 @@
 import Project from "./Project";
-import { findTasks } from "./TaskUi";
+import Task from "./Task";
 
 const ProjectUI = () => {
   // project names container
+  // .add will create a task and add to corresponding project
   let allProjects = [];
   let homeProject = new Project("Home");
   homeProject.add("laundry", "do laundry", "2023-05-01", "low");
+  homeProject.add("cooking", "Make Dinner", "2023-04-01", "high");
+  homeProject.add("roof leak", "repair leak", "2023-04-01", "high");
+
   let workProject = new Project("work");
   workProject.add("report", "ninja report", "2023-04-01", "high");
+
   allProjects.push(homeProject);
   allProjects.push(workProject);
 
+  const projectTasks = document.querySelector("#project-tasks");
   const projectListNames = document.querySelector("#project-list");
   // add project form
   const projectForm = document.querySelector("#project-form");
@@ -32,13 +38,31 @@ const ProjectUI = () => {
     });
   };
 
+  const displayTasks = (tasks) => {
+    const taskList = document.createElement('ul')
+    tasks.forEach((task) => {
+      let listItem = document.createElement('li');
+      listItem.textContent = `${task.name} - ${task.description} - ${task.dueDate} - ${task.priority}`
+      taskList.appendChild(listItem);
+    })
+    projectTasks.appendChild(taskList);
+  };
+
+  const findTasks = (projects, target) => {
+    projects.forEach((project) => {
+      if (project.name.toLowerCase() === target) {
+        displayTasks(project.projectList);
+      }
+    });
+  };
+
   // ****** EVENTS *****
   addProjectButton.addEventListener("click", (e) => {
     projectForm.classList.remove("hidden");
   });
 
   projectListNames.addEventListener("click", (e) => {
-   findTasks(allProjects, e.target.textContent.toLowerCase())
+    findTasks(allProjects, e.target.textContent.toLowerCase());
   });
 
   projectForm.addEventListener("submit", (e) => {
--- src/components/TaskUI.js
+++
@@ -1,9 +0,0 @@
-const findTasks = (projects, target) => {
-  projects.forEach((project) => {
-    if (project.name.toLowerCase() === target) {
-      console.log(project.projectList);
-    }
-  });
-};
-
-export { findTasks };
