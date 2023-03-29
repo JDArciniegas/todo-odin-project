6b65f4c5eb6dba2093be8790ef022a13d81c5cd2
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-03-28 22:47:51 -0600

    added new task modal

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -60,6 +60,32 @@ const Layout = () => {
     }
   };
 
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  addTaskButton.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
   const styleByPriority = (priorityValue, priorityText) => {
     if (priorityValue.textContent === "priority_high") {
       priorityValue.classList.add("high");
@@ -147,10 +173,10 @@ const Layout = () => {
     displayProjectNames(allProjects);
   });
 
-  addTaskButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    taskForm.classList.remove("hidden");
-  });
+  // addTaskButton.addEventListener("click", (e) => {
+  //   e.preventDefault();
+  //   taskForm.classList.remove("hidden");
+  // });
 
   taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
